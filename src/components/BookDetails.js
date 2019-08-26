783968eb9f12bacb80d5d55b8b727e16e90cf619
David Evhade <david@kobo360.com>
2019-08-26 11:47:08 +0100

    finished hooks and context

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -3,9 +3,13 @@ import { BookContext } from '../context/BookContext';
 
 const BookDetails = ({ book }) => {
 
-    const { removeBook } = useContext(BookContext);
+    // const { removeBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
+
     return (
-        <li onClick={() => removeBook(book.id)}>
+        // <li onClick={() => removeBook(book.id)}>
+        <li onClick={() => dispatch({ type: 'REMOVE_BOOK', id: book.id})}>
+
             <div className="title">{book.title}</div>
             <div className="author">{book.author}</div>
 
--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,24 +1,36 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid';
+// import React, { createContext, useState } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
+
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [ books, setBooks ] = useState([
-        {title: "RichDad, PoorDad", author: "Robert Kiyosaki", id: 1},
-        {title: "The Alchemist", author: "Paulo Coehlo", id: 2},
+    // const [ books, setBooks ] = useState([
+    //     {title: "RichDad, PoorDad", author: "Robert Kiyosaki", id: 1},
+    //     {title: "The Alchemist", author: "Paulo Coehlo", id: 2},
 
-    ])
+    // ])
 
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuid()}])
-    }
+    // const addBook = (title, author) => {
+    //     setBooks([{title, author, id: uuid()}, ...books, ])
+    // }
 
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    }
+    // const removeBook = (id) => {
+    //     setBooks(books.filter(book => book.id !== id))
+    // }
+
+    const [ books, dispatch ] = useReducer(bookReducer,  [], () => {
+        return localStorage.books ? JSON.parse(localStorage.books) : []
+    })
+
+    useEffect(() => {
+        localStorage.books = JSON.stringify(books)
+    }, [books])
+
+   
     return (  
-        <BookContext.Provider value={{ books, addBook, removeBook}}>
+        <BookContext.Provider value={{ books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     );
